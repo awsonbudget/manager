735f0446f17102f5d698bb20b83ebacb0acfe64b
Joey Yu <joey@itsjoeoui.com>
2023-02-03 13:07:37 -0500

    Support pod POST and DELETE

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
+from typing import Optional
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
 app.debug = True
+CORS(app)
+
+CLUSTER = "http://localhost:5555"
 
 
 @app.route("/")
 def hello_world():
-    r = requests.get("http://localhost:5555")
+    r = requests.get(CLUSTER)
     return jsonify(r.json())
+
+
+@app.route("/cloud/pod/<string:name>", methods=["POST", "DELETE"])
+def pod(name: Optional[str] = None) -> Response:
+    if request.method == "POST":
+        assert name != None
+        data = requests.post(CLUSTER + "/cloud/pod/" + name)
+        return data.json()
+
+    if request.method == "DELETE":
+        assert name != None
+        data = requests.delete(CLUSTER + "/cloud/pod/" + name)
+        return data.json()
+
+    return jsonify(status=False, msg="manager: what the hell is happenning")
 
 
 if __name__ == "__main__":
