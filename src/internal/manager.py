e4787f0b814f7d3aeb7d6fcf3ff6c82f67a4d0a2
Joey Yu <joey@itsjoeoui.com>
2023-02-26 23:42:21 -0500

    Revert "Make sure node status is also getting updated"
    This reverts commit afef6e60de14db142427eb9407e44a769cfd7e78.

--- src/internal/manager.py
+++ src/internal/manager.py
@@ -62,7 +62,6 @@ class Manager(object):
                     print(f"Node: {job.node}")
                     print("--------------------")
                     await update(WsType.JOB)
-                    await update(WsType.NODE)
             else:
                 print(f"{count}: waiting for jobs")
 
