ed94555d02ea03684ec0155acb3ba31995c89d15
Joey Yu <joey@itsjoeoui.com>
2023-03-19 17:04:48 -0400

    Split dev and prod

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,7 +1,12 @@
+import os
 import json
+
 from src.internal.manager import Manager
 
 manager = Manager()
 
-with open("config.json") as f:
+config_file = "prod-config.json" if os.environ.get("PROD") else "dev-config.json"
+print(f"Using config file: {config_file}")
+
+with open(config_file) as f:
     cluster_group = json.load(f)["cluster_group"]
