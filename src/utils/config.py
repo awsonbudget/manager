dec01aff7da0bda4f2a59aa0f5f603ec6d53af84
Joey Yu <joey@itsjoeoui.com>
2023-03-19 17:14:15 -0400

    nvm revert back

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,12 +1,8 @@
-import os
 import json
 
 from src.internal.manager import Manager
 
 manager = Manager()
 
-config_file = "prod-config.json" if os.environ.get("PROD") else "dev-config.json"
-print(f"Using config file: {config_file}")
-
-with open(config_file) as f:
+with open("config.json") as f:
     cluster_group = json.load(f)["cluster_group"]
