daa56a2e5151737d3c8c4d1e7818f035ab04673a
Joey Yu <joey@itsjoeoui.com>
2023-03-21 09:45:25 -0400

    server ls changes

--- src/routers/server.py
+++ src/routers/server.py
@@ -13,8 +13,26 @@ router = APIRouter(tags=["server"])
 
 
 @router.get("/cloud/server/", dependencies=[Depends(verify_setup)])
-async def server_ls(pod_id: str | None = None) -> Resp:
-    return Resp(status=False)
+async def server_ls(
+    background_tasks: BackgroundTasks, pod_id: str, node_id: str | None = None
+) -> Resp:
+    try:
+        location = manager.get_pod_location(pod_id)
+    except Exception as e:
+        print(e)
+        return Resp(status=False, msg=f"manager: {e}")
+
+    resp = requests.get(
+        cluster_group[location.get_cluster_type()][location.get_cluster_id()]
+        + "/cloud/server/",
+        params={"pod_id": pod_id, "node_id": node_id},
+    ).json()
+    if resp["status"] == False:
+        return Resp(status=False, msg=resp["msg"])
+
+    print(resp["data"])
+    background_tasks.add_task(update, WsType.NODE)
+    return Resp(status=True, msg=resp["msg"], data=resp["data"])
 
 
 @router.post("/cloud/server/launch/", dependencies=[Depends(verify_setup)])
