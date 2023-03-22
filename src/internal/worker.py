6e1470cf2a544a7abeb02ba54cbfd32020316b46
Joey Yu <joey@itsjoeoui.com>
2023-03-21 22:21:18 -0400

    try this

--- src/internal/worker.py
+++ src/internal/worker.py
@@ -6,37 +6,39 @@ from src.utils.ws import update
 
 
 async def main_worker():
-    # FIXME: Fix worker to support multiple clusters
+    # FIXME: Fix worker to support non-default pods
     count = 0
     while True:
         print(manager.queue)
         await asyncio.sleep(3)
         count += 1
         if manager.queue:
-            res = requests.get(
-                cluster_group["heavy"]["default"] + "/internal/available"
-            ).json()
-            if res["status"]:
-                job = manager.queue.popleft()
-                job.status = Status.RUNNING
-                print("--------------------")
-                with open(f"tmp/{job.id}.sh") as f:
-                    res = requests.post(
-                        cluster_group["heavy"]["default"] + "/cloud/job/",
-                        params={
-                            "job_name": job.name,
-                            "job_id": job.id,
-                        },
-                        files={"job_script": f},
-                    ).json()
-                    manager.jobs[job.id].node = res["data"]["node_id"]
+            for cluster_type in cluster_group.keys():
+                res = requests.get(
+                    cluster_group[cluster_type]["default"] + "/internal/available"
+                ).json()
+                if res["status"]:
+                    job = manager.queue.popleft()
+                    job.status = Status.RUNNING
+                    print("--------------------")
+                    with open(f"tmp/{job.id}.sh") as f:
+                        res = requests.post(
+                            cluster_group[cluster_type]["default"] + "/cloud/job/",
+                            params={
+                                "job_name": job.name,
+                                "job_id": job.id,
+                            },
+                            files={"job_script": f},
+                        ).json()
+                        print(res)
+                        manager.jobs[job.id].node = res["data"]["node_id"]
 
-                print("Job allocated")
-                print(f"Name: {job.name}")
-                print(f"ID: {job.id}")
-                print(f"Node: {job.node}")
-                print("--------------------")
-                await update(WsType.JOB)
-                await update(WsType.NODE)
+                    print("Job allocated")
+                    print(f"Name: {job.name}")
+                    print(f"ID: {job.id}")
+                    print(f"Node: {job.node}")
+                    print("--------------------")
+                    await update(WsType.JOB)
+                    await update(WsType.NODE)
         else:
             print(f"{count}: waiting for jobs")
