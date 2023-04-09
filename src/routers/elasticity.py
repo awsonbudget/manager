6a19793b0f655f1c249ee322cb841f331724a130
Joey Yu <joey@itsjoeoui.com>
2023-04-08 20:51:11 -0400

    Fix derps
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/routers/elasticity.py
+++ src/routers/elasticity.py
@@ -1,16 +1,17 @@
 import httpx
 
-from fastapi import APIRouter, Depends
-from src.internal.type import Resp
+from fastapi import APIRouter, BackgroundTasks, Depends
+from src.internal.type import Resp, WsType
 from src.utils.config import manager, cluster_group
 from src.internal.auth import verify_setup
+from src.utils.ws import update
 
 
 router = APIRouter(tags=["elasticity"])
 
 
 @router.post("/cloud/elasticity/lower/", dependencies=[Depends(verify_setup)])
-async def set_lower_threshold(pod_id: str, lower_threshold: int):
+async def set_lower_threshold(pod_id: str, lower_threshold: int) -> Resp:
     try:
         location = manager.get_pod_location(pod_id)
     except Exception as e:
@@ -21,7 +22,7 @@ async def set_lower_threshold(pod_id: str, lower_threshold: int):
         base_url=cluster_group[location.get_cluster_type()][location.get_cluster_id()]
     ) as client:
         resp = (
-            await client.get(
+            await client.post(
                 "/cloud/elasticity/lower/",
                 params={"lower_threshold": lower_threshold, "pod_id": pod_id},
             )
@@ -32,7 +33,7 @@ async def set_lower_threshold(pod_id: str, lower_threshold: int):
 
 
 @router.post("/cloud/elasticity/upper/", dependencies=[Depends(verify_setup)])
-async def set_upper_threshold(pod_id: str, upper_threshold: int):
+async def set_upper_threshold(pod_id: str, upper_threshold: int) -> Resp:
     try:
         location = manager.get_pod_location(pod_id)
     except Exception as e:
@@ -43,7 +44,7 @@ async def set_upper_threshold(pod_id: str, upper_threshold: int):
         base_url=cluster_group[location.get_cluster_type()][location.get_cluster_id()]
     ) as client:
         resp = (
-            await client.get(
+            await client.post(
                 "/cloud/elasticity/upper/",
                 params={"upper_threshold": upper_threshold, "pod_id": pod_id},
             )
@@ -54,7 +55,9 @@ async def set_upper_threshold(pod_id: str, upper_threshold: int):
 
 
 @router.post("/cloud/elasticity/enable/", dependencies=[Depends(verify_setup)])
-async def enable(pod_id: str, min_node: int, max_node: int):
+async def enable(
+    background_tasks: BackgroundTasks, pod_id: str, min_node: int, max_node: int
+) -> Resp:
     try:
         location = manager.get_pod_location(pod_id)
     except Exception as e:
@@ -65,7 +68,7 @@ async def enable(pod_id: str, min_node: int, max_node: int):
         base_url=cluster_group[location.get_cluster_type()][location.get_cluster_id()]
     ) as client:
         resp = (
-            await client.get(
+            await client.post(
                 "/cloud/elasticity/enable/",
                 params={"pod_id": pod_id, "min_node": min_node, "max_node": max_node},
                 timeout=None,
@@ -73,11 +76,12 @@ async def enable(pod_id: str, min_node: int, max_node: int):
         ).json()
         if resp["status"] == False:
             return Resp(status=False, msg=f"manager: {resp['msg']}")
+        background_tasks.add_task(update, WsType.POD)
         return Resp(status=True)
 
 
 @router.post("/cloud/elasticity/disable/", dependencies=[Depends(verify_setup)])
-async def disable(pod_id: str):
+async def disable(pod_id: str) -> Resp:
     try:
         location = manager.get_pod_location(pod_id)
     except Exception as e:
@@ -88,7 +92,7 @@ async def disable(pod_id: str):
         base_url=cluster_group[location.get_cluster_type()][location.get_cluster_id()]
     ) as client:
         resp = (
-            await client.get("/cloud/elasticity/disable/", params={"pod_id": pod_id})
+            await client.post("/cloud/elasticity/disable/", params={"pod_id": pod_id})
         ).json()
         if resp["status"] == False:
             return Resp(status=False)
